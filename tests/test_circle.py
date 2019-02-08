3b6272ae2230d61ff032b740a0e3fce9a19154b2
Greg Echelberger <greg@keystonetowersystems.com>
2019-02-08 11:36:43 -0700

    added contains point test for circle

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,6 +1,7 @@
 import pytest
+import math
 
-from cycart import Circle, P2
+from cycart import Circle, P2, V2
 
 def test_circle_create():
 
@@ -16,3 +17,29 @@ def test_circle_create():
 
     with pytest.raises(TypeError):
         Circle(None)
+
+def test_circle_properties():
+    circle = Circle(100)
+
+    assert circle.diameter == 200
+    assert circle.circumference == pytest.approx(math.pi * 200)
+    assert circle.area == pytest.approx(math.pi * 100 ** 2)
+
+def test_circle_contains():
+    circle = Circle(100)
+
+
+    assert circle.contains(P2(-100, 0))
+    assert circle.contains(P2(100, 0))
+    assert circle.contains(P2(0, 100))
+    assert circle.contains(P2(0, -100))
+
+
+    assert not circle.contains(P2(150, 0))
+    assert not circle.contains(P2(0, 150))
+
+    with pytest.raises(TypeError):
+        circle.contains(None)
+
+    with pytest.raises(TypeError):
+        circle.contains(V2(0, 0))
